d915ba8d7e952b20d15b7eb4cf1faf77d901822d
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-29 10:14:41 +0530

    Update events.py

--- TGN/events.py
+++ TGN/events.py
@@ -88,10 +88,6 @@ def command(**args):
         if "allow_edited_updates" in args:
             del args["allow_edited_updates"]
 
-    def decorator(func):
-        telethn.add_event_handler(func, events.ChatAction(**args))
-        return func
-
     return decorator
 
 def chataction(**args):
