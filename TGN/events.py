0c7d91f44e0f2d3edfcbb28d0249d74522608fdf
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-29 10:06:42 +0530

    Update events.py

--- TGN/events.py
+++ TGN/events.py
@@ -89,16 +89,10 @@ def command(**args):
             del args["allow_edited_updates"]
 
         def decorator(func):
-            if allow_edited_updates:
-                bot.add_event_handler(func, events.MessageEdited(**args))
-            bot.add_event_handler(func, events.NewMessage(**args))
-            try:
-                LOAD_PLUG[file_test].append(func)
-            except:
-                LOAD_PLUG.update({file_test: [func]})
-            return func
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
 
-        return decorator
+    return decorator
 
 def chataction(**args):
     """ Registers chat actions. """
