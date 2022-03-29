5fb58d054ec542ccbcfeb5bdee0eedb4ff3fcdba
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-29 09:33:16 +0530

    Update Gmute.py

--- TGN/modules/Gmute.py
+++ TGN/modules/Gmute.py
@@ -10,6 +10,7 @@ import asyncio
 
 from telethon.events import ChatAction, NewMessage
 from telethon import events
+from TGN.events import register
 from telethon.tl.types import MessageEntityMentionName
 from TGN.modules.sql.users_sql import get_all_chats as get_all_admin_chats
 from TGN.events import is_admin
