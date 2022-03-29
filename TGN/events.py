696992252ad12bafd03205c6b35f6ea80e374513
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-29 08:55:54 +0530

    Update events.py

--- TGN/events.py
+++ TGN/events.py
@@ -174,3 +174,24 @@ def Emli(**args):
     if pattern:
         if not ignore_unsafe:
             args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
+
+async def get_all_admin_chats(event):
+    lul_stark = []
+    all_chats = [
+        d.entity
+        for d in await event.client.get_dialogs()
+        if (d.is_group or d.is_channel)
+    ]
+    for i in all_chats:
+        if i.creator or i.admin_rights:
+            lul_stark.append(i.id)
+    return lul_stark
+
+
+async def is_admin(event, user):
+    sed = await event.client.get_permissions(event.chat_id, user)
+    if sed.is_admin:
+        is_mod = True
+    else:
+        is_mod = False
+    return is_mod
