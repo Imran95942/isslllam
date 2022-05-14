cac71038308b7a795d565a9098b2c18c14fc3d6d
Imran95942 <halil-223@mail.ru>
2022-05-14 10:48:31 +0300

    Update admin.py

--- TGN/modules/admin.py
+++ TGN/modules/admin.py
@@ -920,31 +920,31 @@ def button(update: Update, context: CallbackContext) -> str:
 
   
 __help__ = """
-*User Commands*:
-❂ /admins*:* list of admins in the chat
-❂ /pinned*:* to get the current pinned message.
+*Команды пользователя*:
+❂ /admins*:* список администраторов в чате
+❂ /pinned*:* получение текущего прикрепленного сообщения.
 
-*The Following Commands are Admins only:* 
-❂ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
-❂ /unpin*:* unpins the currently pinned message
-❂ /invitelink*:* gets invitelink
-❂ /promote*:* promotes the user replied to
-❂ /fullpromote*:* promotes the user replied to with full rights
-❂ /demote*:* demotes the user replied to
-❂ /title <title here>*:* sets a custom title for an admin that the bot promoted
-❂ /admincache*:* force refresh the admins list
-❂ /del*:* deletes the message you replied to
-❂ /purge*:* deletes all messages between this and the replied to message.
-❂ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
-❂ /setgtitle <text>*:* set group title
-❂ /setgpic*:* reply to an image to set as group photo
-❂ /setdesc*:* Set group description
-❂ /setsticker*:* Set group sticker
+*Следующие команды доступны только администраторам:* 
+❂ /pin*:* молча прикрепляет сообщение, на которое ответили - добавьте `'loud'` или `'notify'` для уведомления пользователей
+❂ /unpin*:* удалить текущее сообщение, на которое был сделан вывод
+❂ /invitelink*:* получить приглашение на ссылку
+❂ /promote*:* продвигает пользователя, на сообщение которого ответили
+❂ /fullpromote*:* продвигает пользователя, которому ответили, с полными правами
+❂ /demote*:* понижает пользователя, которому ответили
+❂ /title <title here>*:* устанавливает пользовательский заголовок для администратора, которого бот продвигал
+❂ /admincache*:* принудительно обновляет список администраторов
+❂ /del*:* удаляет сообщение, на которое вы ответили
+❂ /purge*:* удаляет все сообщения между этим и отвеченным сообщением.
+❂ /purge <целое число X>*:* удаляет ответное сообщение и X сообщений, следующих за ним, если ответили на сообщение.
+❂ /setgtitle <текст>*:* устанавливает заголовок группы.
+❂ /setgpic*:* ответ на изображение для установки в качестве группового фото
+❂ /setdesc*:* установить описание группы
+❂ /setsticker*:* Установить стикер группы
 
-*Rules*:
-❂ /rules*:* get the rules for this chat.
-❂ /setrules <your rules here>*:* set the rules for this chat.
-❂ /clearrules*:* clear the rules for this chat.
+*Правила*:
+❂ /rules*:* получить правила для этого чата.
+❂ /setrules <ваши правила здесь>*:* установить правила для этого чата.
+❂ /clearrules*:* очистить правила для этого чата.
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
